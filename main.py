3832280a193da09c00a8b6095b605110946021d5
Aminul Zisan <113408314+aminulzisan@users.noreply.github.com>
2025-08-22 19:55:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, dsss!"
+    return "Hello, dsshhs!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use platform-assigned PORT
